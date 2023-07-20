cf99a4afd7919df8b8f673b450a21a680972fb5f
Aryan <raj.aryan.aru@gmail.com>
2023-07-20 17:00:03 +0530

    avator

--- src/components/usernav/Avator.jsx
+++ src/components/usernav/Avator.jsx
@@ -19,7 +19,7 @@ export function Avatar() {
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarFallback>Ar</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
